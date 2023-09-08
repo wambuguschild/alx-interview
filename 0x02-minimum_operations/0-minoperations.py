f873985212b582cd93b4a0741374f63a6dc2c37d
Jedidah <jedidahwambuguw@gmail.com>
2023-09-08 17:42:43 +0300

    Fixed errors

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-""" Module for 0-minoperations"""
-
+""" Module for minOperations """
 
 def minOperations(n):
     """
     minOperations
-    Gets fewest # of operations needed to result in exactly n H characters
+    Gets the fewest number of operations needed to result in exactly n H characters.
     """
     if n <= 1:
         return 0
 
     operations = [0] * (n + 1)
     for i in range(2, n + 1):
-        operations[i] = i  # Initialize with maximum possible operations
+        operations[i] = i
         for j in range(i - 1, 1, -1):
             if i % j == 0:
                 operations[i] = operations[j] + (i // j)
                 break
 
     return operations[n]
+
