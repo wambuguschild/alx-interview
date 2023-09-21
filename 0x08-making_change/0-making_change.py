e3c187adb3552f0fdd4a63af8ea948a40336a83e
Jedidah <jedidahwambuguw@gmail.com>
2023-09-22 01:15:47 +0300

    Added 0. Change comes from within

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -19,14 +19,23 @@ def makeChange(coins, total):
         - You can assume you have an infinite number of each denomination of
         coin in the list
     """
-    if total < 0:
+    if total <= 0:
         return 0
 
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
+    newVal = total + 1
+    store = {0: 0}
 
-    for coin in coins:
-        for sub_total in range(coin, total + 1):
-            dp[sub_total] = min(dp[sub_total], dp[sub_total - coin] + 1)
+    for i in range(1, total + 1):
+        store[i] = newVal
 
-    return dp[total] if dp[total] != float('inf') else -1
+        for coin in coins:
+            current = i - coin
+            if current < 0:
+                continue
+
+            store[i] = min(store[current] + 1, store[i])
+
+    if store[total] == total + 1:
+        return -1
+
+    return store[total]
